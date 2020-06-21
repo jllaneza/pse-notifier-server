9dfa5ea07c53b8cccee763dcd62ae91c9c84f15c
Martin Llaneza <martinllaneza@gmail.com>
2020-06-21 21:30:51 +0800

    Execute job on schedule using node-cron - enhance stock prices list to get from phisix api in case of scraping issue

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 const admin = require('firebase-admin');
+const cron = require('node-cron');
 const serviceAccount = require('./serviceAccountKey.json');
-const { getStockCurrentPrice } = require('./scraper');
+const { getStocksCurrentPrice } = require('./scraper');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -14,12 +15,8 @@ const { getAlerts } = require('./db')(admin);
 const getAlertNotifications = async () => {
   const alerts = await getAlerts();
   const stocks = [...new Set(alerts.map(alert => alert.symbol))];
-  const prices = await Promise.all(stocks.map(getStockCurrentPrice));
-  const stockPrices = stocks.reduce((accumulator, currentValue, index) => {
-    accumulator[currentValue] = prices[index];
-    return accumulator;
-  }, {});
-  
+  const stockPrices = await getStocksCurrentPrice(stocks);
+  console.log(stockPrices);
   return alerts.filter(alert => {
     const { symbol, price, isAbove } = alert;
     const currentPrice = stockPrices[symbol];
@@ -28,10 +25,14 @@ const getAlertNotifications = async () => {
       return currentPrice >= price;
     }
     return currentPrice <= price;
-  }); 
+  });
 }
 
-getAlertNotifications()
-  .then(notifications => {
-    // TODO: send the notifications to respective devices
-  });
+// schedule job to run every 15 seconds on weekdays from 9am - 1pm
+cron.schedule('*/15 * 9-12 * * 1-5', () => {
+  getAlertNotifications()
+    .then(notifications => {
+      // TODO: send the notifications to respective devices
+      console.log(notifications)
+    });
+});
--- scraper.js
+++ scraper.js
@@ -2,14 +2,44 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const url = 'https://www.investagrams.com/Stock/';
+const phisixBaseUrl = 'http://phisix-api4.appspot.com/';
 
-const getStockCurrentPrice = async (stockCode) => {
+const getCurrentPriceOfStock = async (stockCode) => {
   const response = await axios(`${url}${stockCode}`);
   const html = response.data;
   const $ = cheerio.load(html);
   return +$('#lblStockLatestLastPrice').text();
 };
 
+const getAllStocksCurrentPrice = async () => {
+  const response = await axios(`${phisixBaseUrl}stocks.json`);
+  return response.data.stock;
+};
+
+const getStocksCurrentPrice = async (stockCodes) => {
+  let stockPrices = {};
+  try {
+    const prices = await Promise.all(stockCodes.map(getStockCurrentPrice));
+    stockPrices = stocks.reduce((accumulator, currentValue, index) => {
+      accumulator[currentValue] = prices[index];
+      return accumulator;
+    }, {});
+  } catch(e) {
+    // get prices from phisix in case of server error/timeout
+    const prices = await getAllStocksCurrentPrice();
+    stockPrices = prices.reduce((accumulator, currentValue) => {
+      const { symbol, price } = currentValue;
+      const { amount } = price;
+
+      if (stockCodes.includes(symbol)) {
+        accumulator[symbol] = amount;
+      }
+      return accumulator;
+    }, {});
+  }
+  return stockPrices;
+};
+
 module.exports = {
-  getStockCurrentPrice
+  getStocksCurrentPrice
 };
